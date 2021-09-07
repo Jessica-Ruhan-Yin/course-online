7f61eed8df2a6d13a4345e0629d05f4bb0dd5338
littlelittleJessica <446067382@qq.com>
2021-09-07 17:31:40 +0800

    3-4 集成mybatis-generator 步骤1：添加idea插件 步骤2：添加server\src\main\resources\generator\generatorConfig.xml 步骤3：创建maven启动命令mybatis-generator:generate -e

--- server/src/main/java/com/course/server/mapper/TestMapper.java
+++ server/src/main/java/com/course/server/mapper/TestMapper.java
@@ -1,16 +1,30 @@
 package com.course.server.mapper;
 
 import com.course.server.domain.Test;
-
+import com.course.server.domain.TestExample;
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
-/**
- * @Description
- * @Author Jessica
- * @Version v
- * @Date 2021/9/7
- */
 public interface TestMapper {
+    long countByExample(TestExample example);
 
-    public List<Test> list();
+    int deleteByExample(TestExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Test record);
+
+    int insertSelective(Test record);
+
+    List<Test> selectByExample(TestExample example);
+
+    Test selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByExample(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByPrimaryKeySelective(Test record);
+
+    int updateByPrimaryKey(Test record);
 }
