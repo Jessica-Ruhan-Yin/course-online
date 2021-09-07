8d1226028c685ac6fea1edaf6cfa2bf2893c3a6b
littlelittleJessica <446067382@qq.com>
2021-09-07 16:33:00 +0800

    3-1 增加mybatis使用示例

---
+++ system/src/main/java/com/course/system/mapper/TestMapper.java
@@ -0,0 +1,16 @@
+package com.course.system.mapper;
+
+import com.course.system.domain.Test;
+
+import java.util.List;
+
+/**
+ * @Description
+ * @Author Jessica
+ * @Version v
+ * @Date 2021/9/7
+ */
+public interface TestMapper {
+
+    public List<Test> list();
+}
