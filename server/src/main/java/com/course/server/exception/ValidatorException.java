b198cc8809a342025dab8d3fa5b4b61f4a034b59
littlelittleJessica <446067382@qq.com>
2021-09-08 23:27:05 +0800

    5-8 1. 增加后端校验工具类ValidatorUtil    2. 增加统一异常处理，ControllerExceptionHandler,关键字：@ControllerAdvice

---
+++ server/src/main/java/com/course/server/exception/ValidatorException.java
@@ -0,0 +1,15 @@
+package com.course.server.exception;
+
+
+/**
+ * @Description
+ * @Author Jessica
+ * @Version v
+ * @Date 2021/9/8
+ */
+public class ValidatorException extends RuntimeException {
+
+    public ValidatorException(String message) {
+        super(message);
+    }
+}
---
+++ server/src/main/java/com/course/server/util/ValidatorUtil.java
@@ -0,0 +1,37 @@
+package com.course.server.util;
+
+import com.course.server.exception.ValidatorException;
+import org.springframework.util.StringUtils;
+
+
+/**
+ * @Description
+ * @Author Jessica
+ * @Version v
+ * @Date 2021/9/8
+ */
+public class ValidatorUtil {
+
+    /**
+     * 空校验（null or ""）
+     */
+    public static void require(String str, String fieldName) {
+        if (StringUtils.isEmpty(str)) {
+            throw new ValidatorException(fieldName + "不能为空");
+        }
+    }
+
+    /**
+     * 长度校验
+     */
+    public static void length(String str, String fieldName, int min, int max) {
+        int length = 0;
+        if (!StringUtils.isEmpty(str)) {
+            length = str.length();
+        }
+        if (length < min || length > max) {
+            throw new ValidatorException(fieldName + "长度" + min + "~" + max + "位");
+        }
+    }
+}
+
