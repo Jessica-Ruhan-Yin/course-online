714bfaa22438dd93a265dfefd8c337b284302cc6
littlelittleJessica <446067382@qq.com>
2021-09-08 08:33:38 +0800

    5-2 点击sidebar菜单实现页面跳转并激活样式

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -353,16 +353,16 @@
         </div><!-- /.sidebar-shortcuts -->
 
         <ul class="nav nav-list">
-          <li class="">
-            <a href="index.html">
+          <li class="" id="welcome-sidebar">
+            <router-link to="/admin/welcome">
               <i class="menu-icon fa fa-tachometer"></i>
               <span class="menu-text"> 欢迎 </span>
-            </a>
+            </router-link>
 
             <b class="arrow"></b>
           </li>
 
-          <li class="active open">
+          <li class="">
             <a href="#" class="dropdown-toggle">
               <i class="menu-icon fa fa-list"></i>
               <span class="menu-text"> 系统管理 </span>
@@ -390,6 +390,29 @@
 
                 <b class="arrow"></b>
               </li>
+            </ul>
+          </li>
+
+          <li class="active open">
+            <a href="#" class="dropdown-toggle">
+              <i class="menu-icon fa fa-list"></i>
+              <span class="menu-text"> 业务管理 </span>
+
+              <b class="arrow fa fa-angle-down"></b>
+            </a>
+
+            <b class="arrow"></b>
+
+            <ul class="submenu">
+              <li class="active" id="business-chapter-sidebar">
+                <router-link to="/admin/business/chapter">
+                  <i class="menu-icon fa fa-caret-right"></i>
+                  大章管理
+                </router-link>
+
+                <b class="arrow"></b>
+              </li>
+
             </ul>
           </li>
 
@@ -448,8 +471,6 @@
 </template>
 
 <script>
-
-
 export default {
   name: "admin",
   mounted: function() {
@@ -460,6 +481,25 @@ export default {
   methods: {
     login () {
       this.$router.push("/admin")
+    },
+
+    /**
+     * jQuery方法
+     * 菜单激活样式（class="active"），点击某一菜单时显示激活状态
+     * @param id 当前点击菜单的id
+     */
+    activeSidebar: function (id) {
+      //兄弟菜单去除active样式，自身增加active样式
+      $("#" + id).siblings().removeClass("active");
+      $("#" + id).siblings().find("li").removeClass("active");
+      $("#" + id).addClass("active");
+
+      //如果有父菜单，父菜单的兄弟菜单去掉open active，父菜单增加open active
+      let parentLi = $("#" + id).parents("li");
+      if (parentLi) {
+        parentLi.siblings().removeClass("open actuve");
+        parentLi.addClass("open active");
+      }
     }
   }
 }
