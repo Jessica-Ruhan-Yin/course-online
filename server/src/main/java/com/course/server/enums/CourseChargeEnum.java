239b7a2272d3a0c97351bc8a96562a371fe4aace
littlelittleJessica <446067382@qq.com>
2021-09-09 16:29:34 +0800

    6-7 增加枚举类，完成基本的课程管理功能

---
+++ server/src/main/java/com/course/server/enums/CourseChargeEnum.java
@@ -0,0 +1,39 @@
+package com.course.server.enums;
+
+/**
+ * @Description
+ * @Author Jessica
+ * @Version v
+ * @Date 2021/9/9
+ */
+public enum CourseChargeEnum {
+
+    CHARGE("C", "收费"),
+    FREE("F", "免费");
+
+    private String code;
+
+    private String desc;
+
+    CourseChargeEnum(String code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
+
---
+++ server/src/main/java/com/course/server/enums/CourseStatusEnum.java
@@ -0,0 +1,39 @@
+package com.course.server.enums;
+
+/**
+ * @Description
+ * @Author Jessica
+ * @Version v
+ * @Date 2021/9/9
+ */
+public enum CourseStatusEnum {
+
+    PUBLISH("P", "发布"),
+    DRAFT("D", "草稿");
+
+    private String code;
+
+    private String desc;
+
+    CourseStatusEnum(String code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
+
