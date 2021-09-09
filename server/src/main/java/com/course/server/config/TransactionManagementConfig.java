998826a27548224acc76cf71c052de4bf6d8083d
littlelittleJessica <446067382@qq.com>
2021-09-09 22:18:10 +0800

    7-2 增加事务配置

---
+++ server/src/main/java/com/course/server/config/TransactionManagementConfig.java
@@ -0,0 +1,16 @@
+package com.course.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @Description
+ * @Author Jessica
+ * @Version v
+ * @Date 2021/9/9
+ */
+
+@EnableTransactionManagement
+@Configuration
+public class TransactionManagementConfig {
+}
