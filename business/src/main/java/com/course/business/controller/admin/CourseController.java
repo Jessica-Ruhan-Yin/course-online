1afaa5d31a5d3623b45fc440cca1f9fe5921b251
littlelittleJessica <446067382@qq.com>
2021-09-10 20:06:59 +0800

    7-5 完成课程内容管理功能

--- business/src/main/java/com/course/business/controller/admin/CourseController.java
+++ business/src/main/java/com/course/business/controller/admin/CourseController.java
@@ -1,9 +1,6 @@
 package com.course.business.controller.admin;
 
-import com.course.server.dto.CourseCategoryDto;
-import com.course.server.dto.CourseDto;
-import com.course.server.dto.PageDto;
-import com.course.server.dto.ResponseDto;
+import com.course.server.dto.*;
 import com.course.server.service.CourseCategoryService;
 import com.course.server.service.CourseService;
 import com.course.server.util.ValidatorUtil;
@@ -68,6 +65,28 @@ public class CourseController {
         ResponseDto responseDto = new ResponseDto();
         List<CourseCategoryDto> dtoList = courseCategoryService.listByCourse(courseId);
         responseDto.setContent(dtoList);
+        return responseDto;
+    }
+
+    /**
+     * 查找课程内容
+     */
+    @GetMapping("/find-content/{id}")
+    public ResponseDto findContent(@PathVariable String id){
+        ResponseDto responseDto = new ResponseDto();
+        CourseContentDto content = courseService.findContent(id);
+        responseDto.setContent(content);
+        return responseDto;
+    }
+
+
+    /**
+     * 保存课程内容，包含新增和修改
+     */
+    @PostMapping("/save-content")
+    public ResponseDto saveContent(@RequestBody CourseContentDto contentDto){
+        ResponseDto responseDto = new ResponseDto();
+        courseService.saveContent(contentDto);
         return responseDto;
     }
 }
