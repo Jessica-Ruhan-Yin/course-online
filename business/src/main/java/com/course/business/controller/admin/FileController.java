e1ea46afae4c3e7589a4031bd6a16086f1fc1611
littlelittleJessica <446067382@qq.com>
2021-09-11 11:52:13 +0800

    8-6 文件上传功能开发：去掉文件管理的新增、修改、删除功能

--- business/src/main/java/com/course/business/controller/admin/FileController.java
+++ business/src/main/java/com/course/business/controller/admin/FileController.java
@@ -1,11 +1,12 @@
 package com.course.business.controller.admin;
 
-import com.course.server.dto.FileDto;
 import com.course.server.dto.PageDto;
 import com.course.server.dto.ResponseDto;
 import com.course.server.service.FileService;
-import com.course.server.util.ValidatorUtil;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -32,25 +33,4 @@ public class FileController {
         return responseDto;
     }
 
-    @PostMapping("/save")
-    public ResponseDto save(@RequestBody FileDto fileDto) {
-
-        // 保存校验
-        ValidatorUtil.require(fileDto.getPath(), "相对路径");
-        ValidatorUtil.length(fileDto.getPath(), "相对路径", 1, 100);
-        ValidatorUtil.length(fileDto.getName(), "文件名", 1, 100);
-        ValidatorUtil.length(fileDto.getSuffix(), "后缀", 1, 10);
-
-        ResponseDto responseDto = new ResponseDto();
-        fileService.save(fileDto);
-        responseDto.setContent(fileDto);
-        return responseDto;
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public ResponseDto delete(@PathVariable String id) {
-        ResponseDto responseDto = new ResponseDto();
-        fileService.delete(id);
-        return responseDto;
-    }
 }
