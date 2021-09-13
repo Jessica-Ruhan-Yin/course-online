706a6f919267e84d099c12d39b6dbc664a3c05e6
littlelittleJessica <446067382@qq.com>
2021-09-13 11:38:19 +0800

    9-2 大文件断点续传与极速秒传：根据文件标识来新增或修改文件记录，修改分片索引从1开始

--- server/src/main/java/com/course/server/service/FileService.java
+++ server/src/main/java/com/course/server/service/FileService.java
@@ -10,11 +10,11 @@ import com.course.server.util.UuidUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
-import java.util.List;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class FileService {
@@ -32,45 +32,62 @@ public class FileService {
         PageInfo<File> pageInfo = new PageInfo<>(fileList);
         pageDto.setTotal(pageInfo.getTotal());
         List<FileDto> fileDtoList = CopyUtil.copyList(fileList, FileDto.class);
-            pageDto.setList(fileDtoList);
-        }
+        pageDto.setList(fileDtoList);
+    }
 
-        /**
-         * 保存，id有值时更新，无值时新增
-         */
-        public void save(FileDto fileDto) {
-            File file = CopyUtil.copy(fileDto, File.class);
-            if (StringUtils.isEmpty(fileDto.getId())) {
-                this.insert(file);
-            } else {
-                this.update(file);
-            }
-        }
-
-        /**
-         * 新增
-         */
-        private void insert(File file) {
-
-            Date now = new Date();
-            file.setCreatedAt(now);
-            file.setUpdatedAt(now);
-            file.setId(UuidUtil.getShortUuid());
-            fileMapper.insert(file);
-        }
-
-        /**
-         * 更新
-         */
-        private void update(File file) {
-            file.setUpdatedAt(new Date());
-            fileMapper.updateByPrimaryKey(file);
-        }
-
-        /**
-         * 删除
-         */
-        public void delete(String id) {
-            fileMapper.deleteByPrimaryKey(id);
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
+    public void save(FileDto fileDto) {
+        File file = CopyUtil.copy(fileDto, File.class);
+        File fileDb = selectByKey(fileDto.getKey());
+        if (fileDb==null) {
+            this.insert(file);
+        } else {
+            fileDb.setShardIndex(fileDto.getShardIndex());
+            this.update(fileDb);
         }
     }
+
+    /**
+     * 新增
+     */
+    private void insert(File file) {
+
+        Date now = new Date();
+        file.setCreatedAt(now);
+        file.setUpdatedAt(now);
+        file.setId(UuidUtil.getShortUuid());
+        fileMapper.insert(file);
+    }
+
+    /**
+     * 更新
+     */
+    private void update(File file) {
+        file.setUpdatedAt(new Date());
+        fileMapper.updateByPrimaryKey(file);
+    }
+
+    /**
+     * 删除
+     */
+    public void delete(String id) {
+        fileMapper.deleteByPrimaryKey(id);
+    }
+
+    /**
+     * 根据key查找数据库有无相关记录
+     */
+    public File selectByKey(String key) {
+        FileExample example = new FileExample();
+        example.createCriteria().andKeyEqualTo(key);
+        List<File> fileList = fileMapper.selectByExample(example);
+        if (CollectionUtils.isEmpty(fileList)) {
+            return null;
+        } else {
+            return fileList.get(0);
+        }
+    }
+
+}
