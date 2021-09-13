df1cd2daba3b8044c7ea68dbff2e240b3a743a9f
littlelittleJessica <446067382@qq.com>
2021-09-13 21:59:45 +0800

    9-5 大文件断点续传与极速秒传：讲师头像和课程封面改为用大文件组件

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -83,11 +83,11 @@
               <div class="form-group">
                 <label class="col-sm-2 control-label">头像</label>
                 <div class="col-sm-10">
-                  <file v-bind:input-id="'image-upload'"
+                  <big-file v-bind:input-id="'image-upload'"
                         v-bind:text="'上传头像'"
                         v-bind:suffixs="['jpg', 'jpeg', 'png']"
                         v-bind:use="FILE_USE.TEACHER.key"
-                        v-bind:after-upload="afterUpload"></file>
+                        v-bind:after-upload="afterUpload"></big-file>
                   <div v-show="teacher.image" class="row">
                     <div class="col-md-4">
                       <img v-bind:src="teacher.image" class="img-responsive">
@@ -127,11 +127,11 @@
 
 <script>
 import Pagination from "@/components/pagination";
-import File from "@/components/file";
+import BigFile from "@/components/big-file";
 
 export default {
   name: "business-teacher",
-  components: {File, Pagination},
+  components: {BigFile, Pagination},
   data: function () {
     return {
       teacher: {}, //该变量用来绑定form表单的数据
