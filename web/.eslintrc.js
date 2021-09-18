66c7c9294e19b8fdfe8f5381b2c4326b3534ab8d
littlelittleJessica <446067382@qq.com>
2021-09-18 22:57:25 +0800

    13-4 网站首页开发，首页显示新上好课真实数据

---
+++ web/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        'eslint:recommended'
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-undef': 'off',
+        'vue/no-unused-vars': 'off',
+        'vue/require-v-for-key': 'off',
+        'no-unused-vars':'off',
+        'vue/valid-v-model':'off',
+        'vue/no-unused-components': 'off',
+        'no-cond-assign': 'off'
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
+};
