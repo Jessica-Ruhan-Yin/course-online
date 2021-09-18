ccc7013f2efc60b61eb67e6364d56ee0da840317
littlelittleJessica <446067382@qq.com>
2021-09-18 23:22:47 +0800

    13-4 网站首页开发，课程card组件增加等级显示， 增加过滤器filter.js

--- web/src/components/the-course.vue
+++ web/src/components/the-course.vue
@@ -12,6 +12,7 @@
                     <span class="badge badge-info"><i class="fa fa-yen"
                                                       aria-hidden="true"></i>&nbsp;{{ course.price }}</span>&nbsp;
           <span class="badge badge-info"><i class="fa fa-user" aria-hidden="true"></i>&nbsp;123</span>&nbsp;
+          <span class="badge badge-info">{{ COURSE_LEVEL | optionKV(course.level) }}</span>
         </div>
       </div>
     </div>
@@ -25,7 +26,9 @@ export default {
     course: {}
   },
   data: function () {
-    return {}
+    return {
+      COURSE_LEVEL: COURSE_LEVEL
+    }
   },
 }
 </script>
