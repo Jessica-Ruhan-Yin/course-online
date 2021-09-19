347d488be170d49dc29125506739504c80d3a2e9
littlelittleJessica <446067382@qq.com>
2021-09-19 23:33:53 +0800

    13-10 会员登录注册功能开发，登录成功后，头部导航显示会员昵称

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -34,7 +34,7 @@
                 </div>
               </li>
             </ul>
-            <span class="text-white">欢迎：</span>
+            <span v-show="loginMember.id" class="text-white pr-3">您好：{{loginMember.name}}</span>
             <button v-on:click="openLoginModal()" class="btn btn-outline-light my-2 my-sm-0" type="submit">登录/注册</button>
           </div>
         </div>
@@ -50,6 +50,17 @@ import TheLogin from "@/components/login";
 export default {
   name: "the-header",
   components: {TheLogin},
+  data:function (){
+    return{
+      loginMember:{}
+    }
+  },
+
+  mounted() {
+    let _this = this;
+    _this.loginMember = Tool.getLoginMember();
+  },
+
   methods:{
     /**
      * 打开登录注册窗口
@@ -57,6 +68,11 @@ export default {
     openLoginModal() {
       let _this = this;
       _this.$refs.loginComponent.openLoginModal();
+    },
+
+    setLoginMember(loginMember){
+      let _this = this;
+      _this.loginMember = loginMember;
     }
   }
 }
