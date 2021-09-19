e7972d6e68781bb7d5f0d069222dfdd1e20c82a6
littlelittleJessica <446067382@qq.com>
2021-09-19 20:12:50 +0800

    13-8 课程详情页面开发，增加视频播放功能

--- web/src/views/detail.vue
+++ web/src/views/detail.vue
@@ -57,7 +57,7 @@
                     <table class="table table-striped">
                       <tr v-for="(s, j) in chapter.sections" class="chapter-section-tr">
                         <td class="col-sm-8 col-xs-12">
-                          <div class="section-title">
+                          <div v-on:click="play(s)" class="section-title">
                             <i class="fa fa-video-camera d-none d-sm-inline"></i>&nbsp;&nbsp;
                             <span class="d-none d-sm-inline">第{{ j + 1 }}节&nbsp;&nbsp;</span>
                             {{ s.title }}
@@ -92,13 +92,16 @@
       </div>
     </div>
 
+    <modal-player ref="modalPlayer"/>
   </main>
 </template>
 
 <script>
 
+import ModalPlayer from "@/components/modal-player";
 export default {
   name: "detail",
+  components: {ModalPlayer},
   data: function () {
     return {
       id: "",
@@ -151,6 +154,14 @@ export default {
       chapter.folded = !chapter.folded;
       // 在v-for里写v-show，只修改属性不起作用，需要$set
       _this.$set(_this.chapters, i, chapter);
+    },
+
+    /**
+     * 播放视频
+     */
+    play(section){
+      let _this = this;
+      _this.$refs.modalPlayer.playVod(section.vod);
     },
   }
 }
