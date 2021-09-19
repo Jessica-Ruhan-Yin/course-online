a173be4caba6ec448f8fb4c34a4c69b9fdacffc8
littlelittleJessica <446067382@qq.com>
2021-09-19 20:55:28 +0800

    13-9 增加会员管理页面，控台的会员管理，不能新增，修改，删除。可以查看会员信息，重置密码，报表统计等操作

--- business/src/main/java/com/course/business/controller/admin/MemberController.java
+++ business/src/main/java/com/course/business/controller/admin/MemberController.java
@@ -1,11 +1,12 @@
 package com.course.business.controller.admin;
 
-import com.course.server.dto.MemberDto;
 import com.course.server.dto.PageDto;
 import com.course.server.dto.ResponseDto;
 import com.course.server.service.MemberService;
-import com.course.server.util.ValidatorUtil;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -29,29 +30,6 @@ public class MemberController {
         ResponseDto responseDto = new ResponseDto();
         memberService.list(pageDto);
         responseDto.setContent(pageDto);
-        return responseDto;
-    }
-
-    @PostMapping("/save")
-    public ResponseDto save(@RequestBody MemberDto memberDto) {
-
-        // 保存校验
-        ValidatorUtil.require(memberDto.getMobile(), "手机号");
-        ValidatorUtil.length(memberDto.getMobile(), "手机号", 1, 11);
-        ValidatorUtil.require(memberDto.getPassword(), "密码");
-        ValidatorUtil.length(memberDto.getName(), "昵称", 1, 50);
-        ValidatorUtil.length(memberDto.getPhoto(), "头像url", 1, 200);
-
-        ResponseDto responseDto = new ResponseDto();
-        memberService.save(memberDto);
-        responseDto.setContent(memberDto);
-        return responseDto;
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public ResponseDto delete(@PathVariable String id) {
-        ResponseDto responseDto = new ResponseDto();
-        memberService.delete(id);
         return responseDto;
     }
 }
