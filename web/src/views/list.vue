4d11844d81a9614b0a39f4cafbbd233845d8dc29
littlelittleJessica <446067382@qq.com>
2021-09-19 10:19:46 +0800

    13-5 课程列表页面开发，前端分页，增加分页组件

--- web/src/views/list.vue
+++ web/src/views/list.vue
@@ -3,6 +3,12 @@
     <div class="album py-5 bg-light">
       <div class="container">
         <div class="row">
+          <div class="col-md-12">
+            <pagination ref="pagination" v-bind:list="listCourse"></pagination>
+          </div>
+        </div>
+        <br>
+        <div class="row">
           <div v-for="o in courses" class="col-md-4">
             <the-course v-bind:course="o"/>
           </div>
@@ -15,10 +21,11 @@
 
 <script>
 import TheCourse from "@/components/the-course";
+import Pagination from "@/components/pagination";
 
 export default {
   name: "list",
-  components: {TheCourse},
+  components: {TheCourse, Pagination},
   data: function () {
     return {
       courses: [],
@@ -27,6 +34,7 @@ export default {
 
   mounted() {
     let _this = this;
+    _this.$refs.pagination.size = 3;
     _this.listCourse();
   },
 
@@ -38,11 +46,12 @@ export default {
       let _this = this;
       _this.$ajax.post("http://127.0.0.1:9000/business/web/course/list", {
         page: page,
-        size: 3,
+        size: _this.$refs.pagination.size,
       }).then((response) => {
         let resp = response.data;
         if (resp.success) {
           _this.courses = resp.content.list;
+          _this.$refs.pagination.render(page, resp.content.total);
         }
       }).catch((response) => {
         console.log("error:", response);
